422c920062214113cb9bb827318003e3c795a9e9
Noe33000 <champignyn@gmail.com>
2017-02-08 10:38:48 +0100

    Modification connect
    ok

--- inc/connect.php
+++ inc/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost","root", "", "arkamitcjfefedb3");
-//$conn = new mysqli('arkamitcjfefedb3.mysql.db', 'arkamitcjfefedb3', 'C6c6f7946fdc', 'arkamitcjfefedb3');
+	$conn = new mysqli("localhost","root", "", "arkanew");
+	//$conn = new mysqli('arkamitcjfefedb3.mysql.db', 'arkamitcjfefedb3', 'C6c6f7946fdc', 'arkamitcjfefedb3');
 
 ?>
